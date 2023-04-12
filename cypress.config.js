eb2647bad1bf492abd5386669e49bbd4559bc180
leonardo.ferreira <lmelo.rsi@digio.com.br>
2023-04-12 17:53:05 -0300

    pequenas mudanças

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     "baseUrl": "http://localhost:3000",
     "viewportWidth": 1440,
     "viewportHeight": 900,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
